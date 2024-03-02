beff6300a4f1b86c7178ce5cb977f1fd7c9fe94e
cerro88 <bcerro245@gmail.com>
2024-03-02 08:41:40 +0100

    000

--- ejercicio_numeros_mayor_menor.py
+++ ejercicio_numeros_mayor_menor.py
@@ -1,6 +1,3 @@
-
-
-
 # se declara una array vacía donde se almacenarán los números que introduzca el usuario
 numeros = []
 #Se le pide un número al usuario para introducir en la lista
@@ -8,18 +5,11 @@ numero_usuario = int(input("Introduce un número en la lista: "))
 #se añade ese número a la array vacía
 numeros.append(numero_usuario)
 #mientras la respuesta del input sea S vuelve a preguntar y almacena el número en la array
-while input("¿Deseas introducir más números? (S/n)") == "S":
+while input("¿Deseas introducir más números? (S/n)") == "(S)":
     #Se le pide un número al usuario para introducir en la lista
     numero_usuario = int(input("Introduce un número en la lista: "))
     #se añade ese número a la array vacía
     numeros.append(numero_usuario)
 #Si eso no se cumple imprime la array
 print(numeros)
-
-
-
-
-
-
-
     
