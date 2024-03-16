4985fdf8cf75a20407720440e93e70c8776251fc
cerro88 <bcerro245@gmail.com>
2024-03-16 11:59:26 +0100

    find

--- python/cadena7.py
+++ python/cadena7.py
@@ -2,3 +2,6 @@
 # de un producto en euros con dos decimales y muestre por 
 #pantalla el número de euros y el número de céntimos del 
 #precio introducido.
+
+precio = input("Introduce el precio del producto con dos decimales:  ")
+print(precio[:precio.find('.')], 'euros y', precio[precio.find('.')+1:], 'céntimos.')
