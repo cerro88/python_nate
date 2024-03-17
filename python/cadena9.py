888f9298800b9697042b7ba77a6ac5160ddc6fb0
cerro88 <bcerro245@gmail.com>
2024-03-17 09:23:53 +0100

    numerar ejercicios

--- python/cadena9.py
+++ python/cadena9.py
@@ -1,4 +1,4 @@
-#Escribir un programa que pregunte el nombre de un producto, 
+#11.-Escribir un programa que pregunte el nombre de un producto, 
 #su precio y un número de unidades y muestre por pantalla una 
 #cadena con el nombre del producto seguido de su precio 
 #unitario con 6 dígitos enteros y 2 decimales, el número de 
